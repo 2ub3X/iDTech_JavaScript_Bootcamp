6c9163c472a31baad5af211d1470017d162d3af4
2ub3X <swizyace@gmail.com>
2022-02-03 21:26:45 -0500

    added working with Basic data types

---
+++ Basic Data Types Activity/BasicDataTypes.js
@@ -0,0 +1,74 @@
+//4-Strings
+var greetings = "Wasgooddd, I finally see that you made it ";
+var WeirdGuysName = 'nunya';
+var unfunnyJoke = "nun of ya business";
+var YourName = 'Player301';
+var setting = 'The Undergr0und'
+
+//3-integers
+let howManyYearsTheundergru0ndsBeenthere = 1001;
+let WeirdGuysAge = 1111;
+let TheYear = 9606
+
+//1-Array
+let weaponsUsed = ['Nothing ', 'Staff ', 'A Note Book '];
+
+// 1-Booleans
+let WeaponsAvailable = true;
+
+//console.log();
+
+console.log(greetings + YourName + ".");
+console.log(" ");
+console.log("What's that, your asking who am I?");
+console.log(" ");
+console.log("My name is " + WeirdGuysName + ".");
+console.log(" ");
+console.log("...");
+console.log(" ");
+console.log(unfunnyJoke + "...");
+console.log(" ");
+console.log("Bet you weren't expecting that huh? ");
+console.log(" ");
+console.log("Now if your wondering where you are right now well, your in the " + setting + ".");
+console.log(" ");
+console.log("And yes... Underground is spelled with a 0.");
+console.log(" ");
+console.log("This place is very old . . . like about " + howManyYearsTheundergru0ndsBeenthere + " years old, I have to check the Time Line Of Life Again.");
+console.log(" ");
+console.log("Im actually one of the ..Nicer.. Guardians of this place");
+console.log(" ");
+console.log("When you sign up to become a Guardian, you get a higher life Span than the normal human being and or other entity.");
+console.log(" ");
+console.log("I personally didn't belive that...");
+console.log(" ");
+console.log("but I start to notice that I have gotton over the age of 200, I am now " + WeirdGuysAge + " years old.");
+console.log(" ");
+console.log("What's that, your asking what the year is? ");
+console.log(" ");
+console.log("currently it is the year " + TheYear + ".");
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log("NOW");
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log("Time for the most IMPORTANT question");
+console.log(" ");
+console.log("Why.   Are.    You.    Here?");
+console.log(" ");
+console.log("The reason for why you are here is to fight, in a arena with other humans and or other entity.");
+console.log(" ");
+console.log("once you claim your trophy, you will be able to go to your regular day life :) ");
+console.log(" ");
+console.log("here are some available weapons, do you think you have a weapon that you can use from these?");
+console.log(" ");
+console.log(YourName + ": " + WeaponsAvailable + ", I think I'll go with the " + weaponsUsed[1] + ".");
+console.log(" ");
+console.log("Alright then " + YourName + ", get in that arena and");
+console.log(" ");
+console.log(" ");
+console.log("GO.             WIN.");
+
+
